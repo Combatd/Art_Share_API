757dcedfbeaff0caefb70b251febbe486f705175
Combatd <combatdstar@gmail.com>
2020-03-19 05:06:16 -0700

    Define destroy, update, Refactor user_params as hidden encapsulated method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,35 @@ class UsersController < ApplicationController
         render json: User.all
     end
 
+    def show
+        render json: User.find(params[:id])
+    end
+
     def create
-        user = User.new(params.require(:user).permit(:name, :email))
+        user = User.new()
         if user.save
-            render json: user
+            render json: user(user_params)
         else
             render json: user.errors.full_messages, status: :unprocessable_entity
         end
     end
 
-    def show
-        render json: User.find(params[:id])
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+
+        render json: user
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+
+        render json: user
+    end
+
+    private
+    def user_params
+        params[:user].permit(:name, :email)
     end
 end
