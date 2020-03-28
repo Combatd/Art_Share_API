0477ec15f3f634246e6f55aeb5b216115272018b
Combatd <combatdstar@gmail.com>
2020-03-27 19:04:00 -0700

    Move add_index actions to change method in Migration CreateArtworks

--- db/migrate/20200327230730_create_artworks.rb
+++ db/migrate/20200327230730_create_artworks.rb
@@ -7,9 +7,10 @@ class CreateArtworks < ActiveRecord::Migration[5.2]
 
       t.timestamps
     end
+    # two separate artists could both have artworks of their own named "Untitled", but a single artist should not be able to have two pieces with that same title.
+    add_index :artworks, [:title, :artist_id], unique: true
+    add_index :artworks, :artist_id
+    add_index :artworks, :image_url, unique: true
   end
-  # two separate artists could both have artworks of their own named "Untitled", but a single artist should not be able to have two pieces with that same title.
-  add_index :artworks, [:title, :artist_id], unique: true
-  add_index :artworks, :artist_id
-  add_index :artworks, :image_url, unique: true
+ 
 end
