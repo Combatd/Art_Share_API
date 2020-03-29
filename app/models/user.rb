96e34f758485779f3f5add457e1e942a4adf06cf
Combatd <combatdstar@gmail.com>
2020-03-29 09:40:07 -0700

    Associate has_many for artworks, artwork_shares, shared_artworks on User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
 
     has_many :artworks, dependent: destroy,
         foreign_key: :artist_id
+
+    has_many :artwork_shares, dependent: destroy,
+        foreign_key: :viewer_id
+    
+    has_many :shared_artworks,
+        through: :artwork_shares,
+        source: :artwork
 end
