fa95a4e91946c796b47a38b7c5c1c5f666bc7860
Combatd <combatdstar@gmail.com>
2020-04-03 23:22:19 -0700

    Define UsersController##show, create, update, destroy

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -8,19 +8,34 @@ class ArtworksController < ApplicationController
     end
 
     def show
-
+        render json: User.find(params[:id])
     end
 
     def create
-
+        artwork = Artwork.new(artwork_params)
+        if artwork.save
+            render json: artwork
+        else
+            render json: artwork.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def update
-
+        artwork = Artwork.find(params[:id])
+        if artwork.update(artwork_params)
+            render json: artwork
+        else
+            render json: artwork.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def destroy
-
+        artwork = Artwork.find(params[:id])
+        if artwork.destroy
+            render json: artwork
+        else
+            render json: artwork.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     private
