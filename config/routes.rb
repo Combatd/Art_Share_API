d31a2c70d96c9e45b9b1c720788b093ab55bf000
Combatd <combatdstar@gmail.com>
2020-04-04 02:18:07 -0700

    Add resources for users, artworks, in routes.rb - will need to nest artworks in users later

--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,14 @@ Rails.application.routes.draw do
   #                         PATCH  /users/:id(.:format)                         users#update
   #                         PUT    /users/:id(.:format)                         users#update
   #                         DELETE /users/:id(.:format)                         users#destroy
-  get 'users', to: 'users#index'
-  post 'users', to: 'users#create'
-  get 'users/new', to: 'users#new', as: 'new_user'
-  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
-  get 'users/:id', to: 'users#show', as: 'user'
-  patch 'users/:id', to: 'users#update'
-  put 'users/:id', to: 'users#update'
-  delete 'users/:id', to: 'users#destroy'
-
+  # get 'users', to: 'users#index'
+  # post 'users', to: 'users#create'
+  # get 'users/new', to: 'users#new', as: 'new_user'
+  # get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+  # get 'users/:id', to: 'users#show', as: 'user'
+  # patch 'users/:id', to: 'users#update'
+  # put 'users/:id', to: 'users#update'
+  # delete 'users/:id', to: 'users#destroy'
+  resources :users, only: [:index, :show, :update, :create, :destroy]
+  resources :artworks, only: [:index, :show, :update, :create, :destroy]
 end
