6468e9b807e2354b2ed178e46913f6aca8d9221e
Combatd <combatdstar@gmail.com>
2020-04-04 20:15:58 -0700

    Debug missing : for :destroy for app/models/user.rb?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
 
-    has_many :artworks, dependent: destroy,
+    has_many :artworks, dependent: :destroy,
         foreign_key: :artist_id
 
-    has_many :artwork_shares, dependent: destroy,
+    has_many :artwork_shares, dependent: :destroy,
         foreign_key: :viewer_id
     
     has_many :shared_artworks,
