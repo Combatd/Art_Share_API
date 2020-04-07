9f884a9bf4f58754a6c270ba754832f147df2a7e
Combatd <combatdstar@gmail.com>
2020-04-07 14:43:46 -0700

    Define CommentsController##index that retriees by user_id, artwork_id, or grabs all

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,31 @@
 class CommentsController < ApplicationController
     def index
-
+        if params[:user_id]
+            render json: Comment.where(user_id: params[:user_id])
+            
+        elsif params[:artwork_id]
+            render json: Comment.where(artwork_id: params[:artwork_id])
+        else
+            render json: Comment.all
+        end
     end
     
     def create
-
+        comment = Comment.new(comments_params)
+        if comment.save
+            render json: comment
+        else
+            render json: comment.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def destroy
-
+        comment = Comment.find(params[:id])
+        if comment.destroy
+            render json: comment
+        else
+            render json: comment.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     private
