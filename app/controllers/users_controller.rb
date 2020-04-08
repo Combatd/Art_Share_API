a45cdfbe70e901a1ece2b33c436110ba85b516f5
Combatd <combatdstar@gmail.com>
2020-04-08 03:49:58 -0700

    Debug typo in UsersController##user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,6 @@ class UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:user).permit(:username)rails
+        params.require(:user).permit(:username)
     end
 end
