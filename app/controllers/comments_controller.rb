bc5aed541e71bf4a99ba8e087f26d8e8b2f915c2
Combatd <combatdstar@gmail.com>
2020-04-08 04:31:25 -0700

    Remove Whitespace from Controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,6 @@ class CommentsController < ApplicationController
     def index
         if params[:user_id]
             render json: Comment.where(user_id: params[:user_id])
-            
         elsif params[:artwork_id]
             render json: Comment.where(artwork_id: params[:artwork_id])
         else
